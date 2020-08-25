0eacee1a683afb06dfc039d1e87da4b38c679eb3
William Yeung <williamyeung1141998@gmail.com>
2020-08-25 18:31:30 -0400

    Inital Project Setup

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by William Yeung on 8/25/20.
+//  Copyright © 2020 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
